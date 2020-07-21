8247c0f8614d9c5e4098182e931c9bd1e4cf25d2
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-07-22 11:36:53 +1200

    create-journal-entry-component
    form created
    TODO: retrieve users from dropdown from backend

--- frontend/src/components/create-journal.component.js
+++ frontend/src/components/create-journal.component.js
@@ -1,10 +1,137 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateJournal extends Component {
+    constructor(props) {
+        super(props);
+
+        // Bind methods to the this
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeTechnique = this.onChangeTechnique.bind(this);
+        this.onChangeRolling = this.onChangeRolling.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        // Set initial state properties
+        this.state = {
+            username: '',
+            technique: '',
+            rolling: '',
+            date: new Date(),
+            users: []
+        }
+    }
+
+    // Method for username drop down (currently a stub)
+    componentDidMount() {
+        this.setState({
+            users: ['test user'],
+            username: 'test user',
+        });
+    }
+
+    // Methods to update the state
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeTechnique(e) {
+        this.setState({
+            technique: e.target.value
+        });
+    }
+
+    onChangeRolling(e) {
+        this.setState({
+            rolling: e.target.value
+        });
+    }
+
+    onChangeDate(date) {
+        this.setState({
+            date: date
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const journal = {
+            username: this.state.username,
+            technique: this.state.technique,
+            duration: this.state.duration,
+            date: this.state.date
+        };
+
+        console.log(journal);
+
+        window.location = '/';
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Create Journal Entry Component!</p>
+                <h3>Create New Journal Entry</h3>
+                <form onSubmit={this.onSubmit}>
+
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}>
+                                {
+                                    this.state.users.map(function(user) {
+                                        return <option
+                                            key={user}
+                                            value={user}>{user}
+                                            </option>;
+                                    })
+                                }
+                            </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Technique: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.technique}
+                            onChange={this.onChangeTechnique}
+                            />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Rolling: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.rolling}
+                            onChange={this.onChangeRolling}
+                            />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                                />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit"
+                            className="btn btn-primary"
+                            value="Create Exercise Log"
+                            />
+                    </div>
+                </form>
             </div>
         )
     }
