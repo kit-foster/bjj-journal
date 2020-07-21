87b4f07cc2c7867cf0cd6c745523aa36c277bc6f
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-07-22 11:03:55 +1200

    component-created
    component stubs created and compiles correctly

---
+++ frontend/src/components/edit-journal.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditJournal extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Edit Journal Entries Component!</p>
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/journal-entries.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class JournalEntries extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Journal Entries Component!</p>
+            </div>
+        )
+    }
+}
