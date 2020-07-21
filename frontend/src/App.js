b4b2d06571bca233cd2350b395f35e35441fb297
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-07-22 10:54:40 +1200

    react-setup
    set up App.js file and created component folder. does not compile.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import JournalEntries from "./components/journal-entries.component";
+import EditJournal from "./components/edit-journal.component";
+import CreateJournal from "./components/create-journal.component";
+import CreateUser from "./components/create-journal.component";
 
 function App() {
   return (
-    <div className="container">
-      Hello World
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={JournalEntries} />
+        <Route path="/edit/:id" component={EditJournal} />
+        <Route path="/create" component={CreateJournal} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
